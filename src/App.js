f6caf325a9680a80df6d506f8c655f2e72a5a370
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-01-22 00:07:29 -0300

    added-fetch-22-songs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [apiURL, setApiUrl] = useState(
-    "https://assets.breatheco.de/apis/sound/"
-  );
+  
+  const [songs, setSongs] = useState([]);
 
-  const [songs, setSongs] = useState([
-    {
-      id: 1,
-      category: "game",
-      name: "Mario Castle",
-      url: "files/mario/songs/castle.mp3",
-    },
-    {
-      id: 2,
-      category: "game",
-      name: "Mario Star",
-      url: "files/mario/songs/hurry-starman.mp3",
-    },
-    {
-      id: 3,
-      category: "game",
-      name: "Mario Overworld",
-      url: "files/mario/songs/overworld.mp3",
-    },
-  ]);
+  const apiURL = 'https://assets.breatheco.de/apis/sound/';
+
+  let fetchSongs = () => {
+    fetch('https://assets.breatheco.de/apis/sound/songs')
+    .then((response) => {
+      return response.json();
+    })
+    .then ((data) => {
+      setSongs((prevState) => {
+        return data;
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const [currentIndex, setCurrentIndex] = useState(null);
 
   let newSongs = songs.map((index, i) => {
-
     return (
       <>
         <li
@@ -38,8 +38,8 @@ function App() {
             setCurrentIndex(i);
             console.log(currentIndex);
             track.current.src = apiURL + index.url;
-            playRef.current.className = 'hiding';
-            pauseRef.current.className = 'btn btn-primary btn-lg';
+            playRef.current.className = "hiding";
+            pauseRef.current.className = "btn btn-primary btn-lg";
           }}
         >
           <span className="count">{i + 1} </span>
@@ -54,24 +54,29 @@ function App() {
   let pauseRef = useRef(null);
 
   function playTrack() {
-    track.current.play();
-    playRef.current.className = 'hiding';
-    pauseRef.current.className = 'btn btn-primary btn-lg';
+    if (currentIndex === null) {
+      playRef.current.className = "btn btn-success btn-lg";
+      pauseRef.current.className = "hiding";
+    } else {
+      track.current.play();
+      playRef.current.className = "hiding";
+      pauseRef.current.className = "btn btn-primary btn-lg";
+    }
   }
 
   function pauseTrack() {
     track.current.pause();
-    pauseRef.current.className = 'hiding';
-    playRef.current.className = 'btn btn-success btn-lg';
+    pauseRef.current.className = "hiding";
+    playRef.current.className = "btn btn-success btn-lg";
   }
 
   function forwardTrack() {
     if (currentIndex !== songs.length - 1) {
-    track.current.src = apiURL + songs[currentIndex + 1].url;
-    setCurrentIndex(currentIndex + 1);
-    playRef.current.className = 'hiding';
-    pauseRef.current.className = 'btn btn-primary btn-lg';
-    }else {
+      track.current.src = apiURL + songs[currentIndex + 1].url;
+      setCurrentIndex(currentIndex + 1);
+      playRef.current.className = "hiding";
+      pauseRef.current.className = "btn btn-primary btn-lg";
+    } else {
       setCurrentIndex(0);
       track.current.src = apiURL + songs[0].url;
     }
@@ -79,17 +84,29 @@ function App() {
 
   function backwardTrack() {
     if (currentIndex !== 0) {
-    track.current.src = apiURL + songs[currentIndex - 1].url;
-    setCurrentIndex(currentIndex - 1);
-    playRef.current.className = 'hiding';
-    pauseRef.current.className = 'btn btn-primary btn-lg';
-    }else if (currentIndex === 0) {
-      track.current.src = apiURL + songs[2].url;
-      setCurrentIndex(2);
+      track.current.src = apiURL + songs[currentIndex - 1].url;
+      setCurrentIndex(currentIndex - 1);
+      playRef.current.className = "hiding";
+      pauseRef.current.className = "btn btn-primary btn-lg";
+    } else if (currentIndex === 0) {
+      track.current.src = apiURL + songs[21].url;
+      setCurrentIndex(21);
     }
   }
 
-  const [currentIndex, setCurrentIndex] = useState(null);
+  function volumeUp() {
+    if (track.current.volume < 0.9) {
+      track.current.volume += 0.2;
+    }
+  } 
+
+  function volumeDown() {
+    if (track.current.volume > 0.1) {
+      track.current.volume = track.current.volume - 0.2;
+    }
+  }
+
+  console.log(songs.length);
 
   return (
     <>
@@ -99,10 +116,26 @@ function App() {
         </div>
         <audio ref={track} src={null} key={null} controls autoPlay />
         <div className="buttonsBar">
+          <button
+            className="btn btn-link btn-lg vDown animate__animated animate__backInLeft"
+            onClick={volumeDown}
+          >
+            <i class="fas fa-volume-down"></i>
+          </button>
+          <button
+            className="btn btn-link btn-lg animate__animated animate__backInLeft"
+            onClick={volumeUp}
+          >
+            <i class="fas fa-volume-up"></i>
+          </button>
           <button className="btn btn-secondary btn-lg" onClick={backwardTrack}>
             <i className="fas fa-backward"></i>
           </button>
-          <button className="btn btn-success btn-lg" onClick={playTrack} ref={playRef}>
+          <button
+            className="btn btn-success btn-lg"
+            onClick={playTrack}
+            ref={playRef}
+          >
             <i className="fas fa-play"></i>
           </button>
           <button className="hiding" onClick={pauseTrack} ref={pauseRef}>
@@ -111,7 +144,7 @@ function App() {
           <button className="btn btn-secondary btn-lg" onClick={forwardTrack}>
             <i className="fas fa-forward"></i>
           </button>
-          <div className='spotify'>
+          <div className="spotify animate__animated animate__backInRight">
             <i class="fab fa-spotify"></i>
           </div>
         </div>
